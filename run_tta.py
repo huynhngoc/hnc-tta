851c82b1a08499a51a576fd0af0c0d7fd9325605
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2025-01-12 17:18:55 +0100

    Added tta code

---
+++ run_tta.py
@@ -0,0 +1,84 @@
+from deoxys.model import load_model
+import numpy as np
+import tensorflow as tf
+import argparse
+import os
+import h5py
+import pandas as pd
+from deoxys.data.preprocessor import preprocessor_from_config
+import json
+
+
+def augment_image(image, preprocessors):
+    for preprocessor in preprocessors:
+        image = preprocessor.transform(image)
+    return image
+
+
+if __name__ == '__main__':
+    gpus = tf.config.list_physical_devices('GPU')
+    if not gpus:
+        raise RuntimeError("GPU Unavailable")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name")
+    parser.add_argument("config")
+    parser.add_argument("--iter", default=1, type=int)
+
+    args, unknown = parser.parse_known_args()
+    base_path = '../results' + args.name
+    with open(args.config, 'r') as file:
+        config = json.load(file)
+    iter = args.iter
+    preprocessors = []
+    for pp_config in config:
+        preprocessors.append(preprocessor_from_config(pp_config))
+
+    print('Base_path:', args.source)
+    print('Augmentation from:', args.name)
+    print('Iteration:', iter)
+
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+
+
+    ous_h5 = '../segmentation/ous_test.h5'
+    ous_csv = '../segmentation/ous_test.csv'
+    maastro_h5 = '../segmentation/maastro_full.h5'
+    maastro_csv = '../segmentation/maastro_full.csv'
+    model_file = '../segmentation/model.h5'
+
+    model = load_model(model_file)
+
+    if not os.path.exists(base_path + '/OUS'):
+        os.makedirs(base_path + '/OUS')
+
+    ous_df = pd.read_csv(ous_csv)
+
+    print('Working on OUS.....')
+    for pid in ous_df.patient_idx:
+        print('PID:', pid)
+        if not os.path.exists(base_path + '/OUS/' + str(pid)):
+            os.makedirs(base_path + '/OUS/' + str(pid))
+        with h5py.File(ous_h5, 'r') as f:
+            images = f['x'][str(pid)][:][None, ...]
+        preds = model.predict(augment_image(images, preprocessors))
+
+        with open(base_path + '/OUS/' + str(pid) + f'/{iter:02d}.npy', 'wb') as f:
+            np.save(f, preds[0])
+
+    if not os.path.exists(base_path + '/MAASTRO'):
+        os.makedirs(base_path + '/MAASTRO')
+
+    maastro_df = pd.read_csv(maastro_csv)
+    print('Working on MAASTRO.....')
+    for pid in maastro_df.patient_idx:
+        print('PID:', pid)
+        if not os.path.exists(base_path + '/MAASTRO/' + str(pid)):
+            os.makedirs(base_path + '/MAASTRO/' + str(pid))
+        with h5py.File(maastro_h5, 'r') as f:
+            images = f['x'][str(pid)][:][None, ...]
+        preds = model.predict(augment_image(images, preprocessors))
+
+        with open(base_path + '/MAASTRO/' + str(pid) + f'/{iter:02d}.npy', 'wb') as f:
+            np.save(f, preds[0])
---
+++ slurm_tta.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --ntasks=1               # 1 core(CPU)
+#SBATCH --nodes=1                # Use 1 node
+#SBATCH --job-name=uncertainty   # sensible name for the job
+#SBATCH --mem=64G                 # Default memory per CPU is 3GB.
+#SBATCH --partition=gpu # Use the verysmallmem-partition for jobs requiring < 10 GB RAM.
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=$USER@nmbu.no # Email me when job is done.
+#SBATCH --mail-type=FAIL
+#SBATCH --output=outputs/tta-%A-%a.out
+#SBATCH --error=outputs/tta-%A-%a.out
+#SBATCH --input=inputs/all-no.in
+
+# If you would like to use more please adjust this.
+
+## Below you can put your scripts
+# If you want to load module
+module load singularity
+
+## Code
+# If data files aren't copied, do so
+#!/bin/bash
+if [ $# -lt 2 ];
+    then
+    printf "Not enough arguments - %d\n" $#
+    exit 0
+    fi
+
+echo "Finished seting up files."
+
+
+# Run experiment
+# export ITER_PER_EPOCH=200
+# export NUM_CPUS=4
+# export RAY_ROOT=$TMPDIR/ray
+export MAX_SAVE_STEP_GB=0
+# rm -rf $TMPDIR/ray/*
+singularity exec --nv deoxys.sif python -u run_tta.py $1 $PROJECTS/ngoc/segmentation --iter $SLURM_ARRAY_TASK_ID
