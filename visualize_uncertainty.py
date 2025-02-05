0cfb3ad1abefca5ff079f6aec8f5b14b4b8594cc
Anine Lome <aninelo@uio.no>
2025-02-05 10:24:41 +0100

    Made new directory for uncertainty map visualization

--- visualize_uncertainty.py
+++ visualize_uncertainty.py
@@ -15,6 +15,20 @@ base_path = '../analysis/' + args.name
 pid = args.pid
 num_tta = args.num_tta  
 
+"""
+if not os.path.exists(base_path + '/OUS_uncertainty_map_visualization'):
+    os.makedirs(base_path + '/OUS_uncertainty_map_visualization')
+if not os.path.exists(base_path + f'/OUS_uncertainty_map_visualization/{num_tta:02d}_{pid}'):
+    os.makedirs(base_path + f'/OUS_uncertainty_map_visualization/{num_tta:02d}_{pid}')
+"""
+
+if not os.path.exists(base_path + '/MAASTRO_uncertainty_map_visualization'):
+    os.makedirs(base_path + '/MAASTRO_uncertainty_map_visualization')
+if not os.path.exists(base_path + f'/MAASTRO_uncertainty_map_visualization/{num_tta:02d}'):
+    os.makedirs(base_path + f'/MAASTRO_uncertainty_map_visualization/{num_tta:02d}')
+
+print(f'Visualizing uncertainty map for PID: {pid} with {num_tta} TTA')
+
 # Load the uncertainty map
 uncertainty_map_path = f'{base_path}/MAASTRO_uncertainty_map/{num_tta:02d}/{pid}.npy'
 uncertainty_map = np.load(uncertainty_map_path)
@@ -31,4 +45,5 @@ plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 
 # Save the figure as a PDF file
-output_path = f'{base_path}/MAASTRO_uncertainty_map/{num_tta:02d}/{pid}_slice.pdf'
+output_path = f'{base_path}/MAASTRO_uncertainty_map_visualization/{num_tta:02d}/pid_{pid}_slice.pdf'
+plt.savefig(output_path, format='pdf')
