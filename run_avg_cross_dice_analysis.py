d4ae78d0fccd1e640a15f60eff2eb60535b65e5b
Anine Lome <aninelo@uio.no>
2025-02-10 17:15:01 +0100

    Fixed small bug

--- run_avg_cross_dice_analysis.py
+++ run_avg_cross_dice_analysis.py
@@ -20,44 +20,45 @@ if __name__ == '__main__':
     MAASTRO_transformed_path = base_path + '/MAASTRO_analysis/MAASTRO_avg_cross_dice_analysis.csv'
     
     # Initialize an empty DataFrame to store the transformed data
-    transformed_df = pd.DataFrame()
+    ous_transformed_df = pd.DataFrame()
+    MAASTRO_transformed_df = pd.DataFrame()
 
     print('Working on OUS.....')
     for i in range(2, num_tta + 1):
         # Read the original CSV file
-        df = pd.read_csv(base_path + f'/OUS_analysis/dice_{i:02d}.csv')
-        df.groupby('pid').agg({'dice': 'mean'}).reset_index()
+        ous_df = pd.read_csv(base_path + f'/OUS_analysis/dice_{i:02d}.csv')
+        ous_df.groupby('pid').agg({'dice': 'mean'}).reset_index()
         # Calculate the mean cross Dice score for each patient
-        mean_dice = df.groupby('pid').agg({'dice': 'mean'}).reset_index()
-        mean_dice.rename(columns={'dice': f'mean_dice_{i:02d}'}, inplace=True)
+        ous_mean_dice = ous_df.groupby('pid').agg({'dice': 'mean'}).reset_index()
+        ous_mean_dice.rename(columns={'dice': f'mean_dice_{i:02d}'}, inplace=True)
         
         # Merge the mean cross Dice scores into the transformed DataFrame
-        if transformed_df.empty:
-            transformed_df = mean_dice
+        if ous_transformed_df.empty:
+            ous_transformed_df = ous_mean_dice
         else:
-            transformed_df = pd.merge(transformed_df, mean_dice, on='pid', how='outer')
+            ous_transformed_df = pd.merge(ous_transformed_df, ous_mean_dice, on='pid', how='outer')
 
     # Save the transformed data to a new CSV file
-    transformed_df.to_csv(OUS_transformed_path, index=False)
+    ous_transformed_df.to_csv(OUS_transformed_path, index=False)
 
     print(f"Transformed data saved to {OUS_transformed_path}")
 
     print('Working on MAASTRO.....')
     for i in range(2, num_tta + 1):
         # Read the original CSV file
-        df = pd.read_csv(base_path + f'/MAASTRO_analysis/dice_{i:02d}.csv')
-        df.groupby('pid').agg({'dice': 'mean'}).reset_index()
+        MAASTRO_df = pd.read_csv(base_path + f'/MAASTRO_analysis/dice_{i:02d}.csv')
+        MAASTRO_df.groupby('pid').agg({'dice': 'mean'}).reset_index()
         # Calculate the mean cross Dice score for each patient
-        mean_dice = df.groupby('pid').agg({'dice': 'mean'}).reset_index()
-        mean_dice.rename(columns={'dice': f'mean_dice_{i:02d}'}, inplace=True)
+        MAASTRO_mean_dice = MAASTRO_df.groupby('pid').agg({'dice': 'mean'}).reset_index()
+        MAASTRO_mean_dice.rename(columns={'dice': f'mean_dice_{i:02d}'}, inplace=True)
         
         # Merge the mean cross Dice scores into the transformed DataFrame
-        if transformed_df.empty:
-            transformed_df = mean_dice
+        if MAASTRO_transformed_df.empty:
+            MAASTRO_transformed_df = MAASTRO_mean_dice
         else:
-            transformed_df = pd.merge(transformed_df, mean_dice, on='pid', how='outer')
+            MAASTRO_transformed_df = pd.merge(MAASTRO_transformed_df, MAASTRO_mean_dice, on='pid', how='outer')
 
     # Save the transformed data to a new CSV file
-    transformed_df.to_csv(MAASTRO_transformed_path, index=False)
+    MAASTRO_transformed_df.to_csv(MAASTRO_transformed_path, index=False)
 
 print(f"Transformed data saved to {MAASTRO_transformed_path}")
