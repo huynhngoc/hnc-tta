4a1100de29a46889fb53866b43c95669701d59a0
Anine Lome <aninelo@uio.no>
2025-02-13 11:24:13 +0100

    Changed plotting details

--- analysis/CrossDiceVisualization/cross_dice_groups_starting_threshold.py
+++ analysis/CrossDiceVisualization/cross_dice_groups_starting_threshold.py
@@ -25,7 +25,7 @@ group1_mean_values = group1[num_tta_cols].mean()
 group2_mean_values = group2[num_tta_cols].mean()
 
 # Create subplots
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 6))
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 8))
 
 # Plot the data for group 1
 for pid in group1['pid'].unique():
@@ -33,9 +33,9 @@ for pid in group1['pid'].unique():
     ax1.plot(num_tta_cols, patient_data, alpha=0.3)
 
 ax1.plot(num_tta_cols, group1_mean_values, label='Mean of group 1', color='black', linewidth=2, linestyle='--', alpha=1.0)
-ax1.set_xlabel('Number of TTA')
+ax1.set_xlabel('Number of TTA-predictions')
 ax1.set_ylabel('Mean Cross Dice Score')
-ax1.set_title('Group 1: Mean Cross Dice Score vs. Number of TTA')
+ax1.set_title(f'Group 1: Mean Cross Dice Score for 2 TTA-predictions < {0.7}')
 ax1.legend(loc='lower left')
 ax1.set_xticks(num_tta_cols)
 ax1.set_xticklabels(new_x_labels)
@@ -46,15 +46,15 @@ for pid in group2['pid'].unique():
     ax2.plot(num_tta_cols, patient_data, alpha=0.3)
 
 ax2.plot(num_tta_cols, group2_mean_values, label='Mean of group 2', color='black', linewidth=2, linestyle='--', alpha=1.0)
-ax2.set_xlabel('Number of TTA')
+ax2.set_xlabel('Number of TTA-predictions')
 ax2.set_ylabel('Mean Cross Dice Score')
-ax2.set_title('Group 2: Mean Cross Dice Score vs. Number of TTA')
+ax2.set_title(f'Group 2: Mean Cross Dice Score for 2 TTA-predictions >= {0.7}')
 ax2.legend(loc='lower left')
 ax2.set_xticks(num_tta_cols)
 ax2.set_xticklabels(new_x_labels)
 
-fig.suptitle('Mean Cross Dice Score vs. Number of TTA for OUS Dataset', fontsize=16)
-plt.tight_layout(rect=[0, 0.03, 1, 0.95])   
+fig.suptitle('OUS Dataset', fontsize=12)
+plt.tight_layout(pad=2.0)   
 
 # Save the plot to a PDF file
 plt.savefig('OUS_Cross_Dice_2Groups.pdf', format='pdf', bbox_inches='tight')
@@ -70,7 +70,7 @@ MAASTRO_df = pd.read_csv('MAASTRO_avg_cross_dice_analysis.csv')
 # Extract the patient IDs
 patient_ids = MAASTRO_df['pid'].unique()
 
-# Extract the time series column names
+# Extract the column names
 num_tta_cols = MAASTRO_df.columns[1:]  # Assuming the first column is 'pid'
 
 # Dividing patients into groups based on starting cross dice value
@@ -82,7 +82,7 @@ group1_mean_values = group1[num_tta_cols].mean()
 group2_mean_values = group2[num_tta_cols].mean()
 
 # Create subplots
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 6))
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 8))
 
 # Plot the data for group 1
 for pid in group1['pid'].unique():
@@ -90,9 +90,9 @@ for pid in group1['pid'].unique():
     ax1.plot(num_tta_cols, patient_data, alpha=0.3)
 
 ax1.plot(num_tta_cols, group1_mean_values, label='Mean of group 1', color='black', linewidth=2, linestyle='--', alpha=1.0)
-ax1.set_xlabel('Number of TTA')
+ax1.set_xlabel('Number of TTA-predictions')
 ax1.set_ylabel('Mean Cross Dice Score')
-ax1.set_title('Group 1: Mean Cross Dice Score vs. Number of TTA')
+ax1.set_title(f'Group 1: Mean Cross Dice Score for 2 TTA-predictions < {0.7}')
 ax1.legend(loc='lower left')
 ax1.set_xticks(num_tta_cols)
 ax1.set_xticklabels(new_x_labels)
@@ -103,15 +103,15 @@ for pid in group2['pid'].unique():
     ax2.plot(num_tta_cols, patient_data, alpha=0.3)
 
 ax2.plot(num_tta_cols, group2_mean_values, label='Mean of group 2', color='black', linewidth=2, linestyle='--', alpha=1.0)
-ax2.set_xlabel('Number of TTA')
+ax2.set_xlabel('Number of TTA-predictions')
 ax2.set_ylabel('Mean Cross Dice Score')
-ax2.set_title('Group 2: Mean Cross Dice Score vs. Number of TTA')
+ax2.set_title(f'Group 2: Mean Cross Dice Score for 2 TTA-predictions >= {0.7}')
 ax2.legend(loc='lower left')
 ax2.set_xticks(num_tta_cols)
 ax2.set_xticklabels(new_x_labels)
 
-fig.suptitle('Mean Cross Dice Score vs. Number of TTA for MAASTRO Dataset', fontsize=16)
-plt.tight_layout(rect=[0, 0.03, 1, 0.95]) 
+fig.suptitle(f'MAASTRO Dataset', fontsize=12)
+plt.tight_layout(pad=2) 
 
 # Save the plot to a PDF file
 plt.savefig('MAASTRO_Cross_Dice_2Groups.pdf', format='pdf', bbox_inches='tight')
--- analysis/CrossDiceVisualization/cross_dice_visualization.py
+++ analysis/CrossDiceVisualization/cross_dice_visualization.py
@@ -16,7 +16,7 @@ num_tta_cols = ous_df.columns[1:]  # Assuming the first column is 'pid'
 # Define new x-axis labels
 new_x_labels = [str(i) for i in range(2, 21)]
 
-# Calculate the mean of all the values in each column
+# Calculate the mean of all the values in each column to plot the mean line
 ous_mean_values = ous_df[num_tta_cols].mean()
 
 # Plot the data
@@ -26,16 +26,16 @@ for pid in patient_ids:
     # Extract the mean cross dice values for the current patient
     patient_data = ous_df[ous_df['pid'] == pid][num_tta_cols].values.flatten()
 
-    # Plot the time series data
+    # Plot the data
     plt.plot(num_tta_cols, patient_data, alpha=0.3)
 
 # Plot the mean values
 plt.plot(num_tta_cols, ous_mean_values, label='Mean of all patients', color='black', linewidth=2, linestyle='--', alpha=1.0)
 
 # Add labels and title
-plt.xlabel('Number of TTA')
+plt.xlabel('Number of TTA-predictions')
 plt.ylabel('Mean Cross Dice Score')
-plt.title('Mean Cross Dice Score vs. Number of TTA for OUS Dataset')
+plt.title('Mean Cross Dice Score vs. Number of TTA-predictions for OUS Dataset')
 plt.legend(loc='lower left')
 
 # Set new x-axis labels
@@ -43,8 +43,6 @@ plt.xticks(ticks=num_tta_cols, labels=new_x_labels)
 
 # Place the legend outside the plot
 #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-
 
 # Save the plot to a PDF file
 # plt.savefig('OUS_Cross_Dice_All_Patients.pdf', format='pdf')
@@ -63,7 +61,7 @@ MAASTRO_df = pd.read_csv('MAASTRO_avg_cross_dice_analysis.csv')
 # Extract the patient IDs
 patient_ids = MAASTRO_df['pid'].unique()
 
-# Extract the time series column names
+# Extract the column names
 num_tta_cols = MAASTRO_df.columns[1:]  # Assuming the first column is 'pid'
 
 # Calculate the mean of all the values in each column
@@ -73,10 +71,10 @@ MAASTRO_mean_values = ous_df[num_tta_cols].mean()
 plt.figure(figsize=(10, 6))
 
 for pid in patient_ids:
-    # Extract the time series values for the current patient
+    # Extract the values for the current patient
     patient_data = MAASTRO_df[MAASTRO_df['pid'] == pid][num_tta_cols].values.flatten()
     
-    # Plot the time series data
+    # Plot the data
     plt.plot(num_tta_cols, patient_data, alpha=0.3)
 
 # Plot the mean values
