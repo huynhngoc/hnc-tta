fa8b94f74540a2bfd6b25fed83f3ddd887090048
Anine Lome <aninelo@uio.no>
2025-02-17 15:41:47 +0100

    Working on patient wise analysis

--- analysis/patient_wise_analysis/patient_wise_visualize.py
+++ analysis/patient_wise_analysis/patient_wise_visualize.py
@@ -3,16 +3,17 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-print('Working on IoU vs original dice score visualization.....')
 
-ous_df = pd.read_csv("OUS_iou_vs_original_dice.csv")
-maastro_df = pd.read_csv("MAASTRO_iou_vs_original_dice.csv")
+ous_df = pd.read_csv("OUS_patient_wise_analysis.csv")
+maastro_df = pd.read_csv("MAASTRO_patient_wise_analysis.csv")
 
 # Combine DataFrames
 ous_df["source"] = "OUS"  # Add identifier column
 maastro_df["source"] = "MAASTRO"  # Add identifier column
 df = pd.concat([ous_df, maastro_df])  # Merge datasets
 
+
+print('Working on IoU vs original dice score visualization.....')
 # Create scatter plot
 plt.figure(figsize=(8, 6))
 for source, subset in df.groupby("source"):
@@ -34,23 +35,6 @@ plt.show()
 
 print('Working on average cross dice score vs original dice score visualization.....')
 
-ous_cross_dice = pd.read_csv("../CrossDiceVisualization/OUS_avg_cross_dice_analysis.csv")
-maastro_cross_dice = pd.read_csv("../CrossDiceVisualization/MAASTRO_avg_cross_dice_analysis.csv")
-
-ous_cross_dice = ous_cross_dice[['pid','mean_dice_15']]
-maastro_cross_dice = maastro_cross_dice[['pid','mean_dice_15']]
-
-
-ous_transformed_df = pd.merge(ous_cross_dice, ous_df, on='pid', how='outer')
-maastro_transformed_df = pd.merge(maastro_cross_dice, maastro_df, on='pid', how='outer')
-
-# Combine DataFrames
-ous_transformed_df["source"] = "OUS"  # Add identifier column
-maastro_transformed_df["source"] = "MAASTRO"  # Add identifier column
-df = pd.concat([ous_transformed_df, maastro_transformed_df])  # Merge datasets
-
-print(df.head())
-print(df.tail())
 # Create scatter plot
 plt.figure(figsize=(8, 6))
 for source, subset in df.groupby("source"):
@@ -66,6 +50,67 @@ plt.grid(True)
 
 # Save the plot to a PDF file
 plt.savefig('crossdice_vs_orgdice.pdf', format='pdf', bbox_inches='tight')
+
+plt.show()
+
+
+print('Working on sum entropy vs original dice score visualization.....')
+
+# Create scatter plot
+plt.figure(figsize=(8, 6))
+for source, subset in df.groupby("source"):
+    plt.scatter(subset["original_dice_score"], subset["sum_entropy"], label=source)#, alpha=0.7)
+
+
+# Labels and title
+plt.xlabel("Original Dice Score")
+plt.ylabel("Sum Entropy")
+plt.title("Scatter Plot of Sum Entropy vs. Original Dice Score")
+plt.legend()
+plt.grid(True)
+
+# Save the plot to a PDF file
+plt.savefig('entropy_vs_orgdice.pdf', format='pdf', bbox_inches='tight')
+
+plt.show()
+
+print('Working on volume vs original dice score visualization.....')
+
+# Create scatter plot
+plt.figure(figsize=(8, 6))
+for source, subset in df.groupby("source"):
+    plt.scatter(subset["original_dice_score"], subset["actual_vol"], label=source)#, alpha=0.7)
+
+
+# Labels and title
+plt.xlabel("Original Dice Score")
+plt.ylabel("Actual Volume")
+plt.title("Scatter Plot of Actual Volume vs. Original Dice Score")
+plt.legend()
+plt.grid(True)
+
+# Save the plot to a PDF file
+plt.savefig('volume_vs_orgdice.pdf', format='pdf', bbox_inches='tight')
+
+plt.show()
+
+print('Working on volume vs uncertainty visualization.....')
+
+# Create scatter plot
+plt.figure(figsize=(8, 6))
+for source, subset in df.groupby("source"):
+    plt.scatter(subset["mean_dice_15"], subset["actual_vol"], label=source)#, alpha=0.7)
+
+
+# Labels and title
+plt.xlabel("Mean Cross Dice Score")
+plt.ylabel("Actual Volume")
+plt.title("Scatter Plot of Actual Volume vs. Mean Cross Dice Score")
+plt.legend()
+plt.grid(True)
+
+# Save the plot to a PDF file
+plt.savefig('volume_vs_crossdice.pdf', format='pdf', bbox_inches='tight')
 
 plt.show()
 
