d4e68425f866f41de864cf5fb1f8f0c47052edcd
Anine Lome <aninelo@uio.no>
2025-02-20 10:45:13 +0100

    visualized new patient

--- analysis/visualize_uncertainty/visualize_uncertainty.py
+++ analysis/visualize_uncertainty/visualize_uncertainty.py
@@ -376,7 +376,7 @@ for i in range(2, num_samples+1):
     union = union + new_pred
 """
 num_samples = 15
-patient_id = 45
+patient_id = 82
 uncertainty_map = np.zeros((173, 191, 265, 1))
 
 folder_path = '/Users/bruker/Desktop/Master'
