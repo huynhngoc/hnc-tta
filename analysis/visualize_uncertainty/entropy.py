bf9a40a9022d1a946aa537fe4788d5d5ad677a05
Anine Lome <aninelo@uio.no>
2025-02-27 16:40:56 +0100

    plot details

--- analysis/visualize_uncertainty/entropy.py
+++ analysis/visualize_uncertainty/entropy.py
@@ -6,17 +6,20 @@ x = np.linspace(0, 1, 100)
 y_e = -x * np.log(x) 
 #y_b = -x * np.log(x) - (1-x) * np.log(1-x)
 
-y_e_max = np.max(y_e)
+y_e_max = 1/np.e
+x_e_max = 1/np.e
 #y_b_max = np.max(y_b)
 
 plt.plot(x, y_e, label='Entropy $E$', color='darkorange')
 #plt.plot(x, y_b)
 
-plt.axvline(x=0.5, color='orchid', linestyle='--', alpha=0.9)
-#plt.axhline(y=y_e_max, color='red', linestyle='--')
+plt.axvline(x=0.5, color='lightseagreen', linestyle='--', alpha=0.4)
+plt.axvline(x=x_e_max, color='orchid', linestyle='--', alpha=0.9)
+
+plt.axhline(y=y_e_max, color='orchid', linestyle='--', alpha=0.9)
 #plt.axhline(y=y_b_max, color='red', linestyle='--')
 
-plt.xlabel('Positive class probability $p$')
+plt.xlabel('Positive class probability $p_i$')
 plt.ylabel('Uncetainty value')
 plt.legend()
 
