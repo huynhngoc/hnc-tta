8345d3a06109cc13771cacf50fdcb6666c7e7a80
Anine Lome <aninelo@uio.no>
2025-03-12 15:41:05 +0100

    debugging

--- visualize_augmentation.py
+++ visualize_augmentation.py
@@ -120,6 +120,6 @@ if center == "OUS":
             os.makedirs(base_path + f'/OUS_augmentation_visualization/{aug_type}')
 
 
-        output_path = f'{base_path}/OUS_augmentation_visualization/{aug_type}/pid{pid}_CT_PET_1_8.pdf'
+        output_path = f'{base_path}/OUS_augmentation_visualization/{aug_type}/pid{pid}_CT_PET_2.pdf'
         plt.savefig(output_path, format='pdf')
         plt.close
