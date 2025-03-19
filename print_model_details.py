d16f4f786676e6fcf1dc58e9d205673e29b8026a
Anine Lome <aninelo@uio.no>
2025-03-19 09:46:22 +0100

    made script for printing model details

---
+++ print_model_details.py
@@ -0,0 +1,21 @@
+from deoxys.model import load_model
+import numpy as np
+import tensorflow as tf
+import argparse
+import os
+import h5py
+import pandas as pd
+from deoxys.data.preprocessor import preprocessor_from_config
+import json
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("source")
+args, unknown = parser.parse_known_args()
+source = args.source
+
+model_file = source + '/segmentation/model.h5'
+
+model = load_model(model_file)
+
+print(model.summary())
