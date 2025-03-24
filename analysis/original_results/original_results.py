e10bd43a9e5b1b76163943ea50b79e1df4b1fabf
Anine Lome <aninelo@uio.no>
2025-03-24 11:43:34 +0100

    find median of original results

--- analysis/original_results/original_results.py
+++ analysis/original_results/original_results.py
@@ -8,8 +8,11 @@ maastro_df = pd.read_csv(f"MAASTRO_original_results.csv")
 
 ous_dsc_mean = ous_df["f1_score"].mean()
 ous_dsc_std = ous_df["f1_score"].std()
+ous_dsc_median = ous_df["f1_score"].median()
+
 maastro_dsc_mean = maastro_df["f1_score"].mean()
 maastro_dsc_std = maastro_df["f1_score"].std()
+maastro_dsc_median = maastro_df["f1_score"].median()
 
-print(f"OUS DSC mean: {ous_dsc_mean:.3f} ± {ous_dsc_std:.3f}")
-print(f"MAASTRO DSC mean: {maastro_dsc_mean:.3f} ± {maastro_dsc_std:.3f}")
+print(f"OUS DSC mean: {ous_dsc_mean:.3f} ± {ous_dsc_std:.3f} (median: {ous_dsc_median:.3f})")
+print(f"MAASTRO DSC mean: {maastro_dsc_mean:.3f} ± {maastro_dsc_std:.3f} (median: {maastro_dsc_median:.3f})")
