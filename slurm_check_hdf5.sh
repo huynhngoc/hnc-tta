a28ac405f072cec215e1006c12349f488962daa9
Anine Lome <aninelo@uio.no>
2025-04-19 11:22:30 +0200

    changed output email

--- slurm_check_hdf5.sh
+++ slurm_check_hdf5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --ntasks=1               # 1 core(CPU)
 #SBATCH --nodes=1                # Use 1 node
-#SBATCH --job-name=SpinalAI_run   # sensible name for the job
-#SBATCH --mem=16G                 # Default memory per CPU is 3GB.
+#SBATCH --job-name=TTA_run   # sensible name for the job
+#SBATCH --mem=1G                 # Default memory per CPU is 3GB.
 #SBATCH --partition=orion         # Use hugemem if you need > 48 GB (check the orion documentation)
 #SBATCH --constraint=avx2
-#SBATCH --mail-user=$USER@nmbu.no # Email me when job is done.
+#SBATCH --mail-user=anine.lome@nmbu.no # Email me when job is done.
 #SBATCH --mail-type=FAIL
 #SBATCH --output=outputs/run_python-%A.out
 #SBATCH --error=outputs/run_python-%A.out
