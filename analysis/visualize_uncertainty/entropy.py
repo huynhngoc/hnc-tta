51591ffd19e5400146a795e743a1df6c3b91d8b5
Anine Lome <aninelo@uio.no>
2025-04-22 18:58:41 +0200

    small change to plot layout

--- analysis/visualize_uncertainty/entropy.py
+++ analysis/visualize_uncertainty/entropy.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.linspace(0, 1, 100)
+x = np.linspace(0, 1, 1000)
 y_e = -x * np.log(x) 
 #y_b = -x * np.log(x) - (1-x) * np.log(1-x)
 
@@ -10,21 +10,21 @@ y_e_max = 1/np.e
 x_e_max = 1/np.e
 #y_b_max = np.max(y_b)
 
-plt.plot(x, y_e, label='Entropy $E$', color='darkorange')
+plt.figure(figsize=(5, 4))
+plt.plot(x, y_e, label='Entropy', color="#d95f02", linewidth=2)
 #plt.plot(x, y_b)
+plt.axvline(x=0.5, color='#b2df8a', linestyle='--', alpha=1)
+plt.axvline(x=x_e_max, color='#7570b3', linestyle='--', alpha=0.9)
 
-plt.axvline(x=0.5, color='lightseagreen', linestyle='--', alpha=0.4)
-plt.axvline(x=x_e_max, color='orchid', linestyle='--', alpha=0.9)
-
-plt.axhline(y=y_e_max, color='orchid', linestyle='--', alpha=0.9)
+plt.axhline(y=y_e_max, color='#7570b3', linestyle='--', alpha=0.9)
 #plt.axhline(y=y_b_max, color='red', linestyle='--')
 
-plt.xlabel('Positive class probability $p_i$')
-plt.ylabel('Uncetainty value')
+plt.xlabel('GTV class probability', fontsize=11)
+plt.ylabel('Uncertainty value', fontsize=11)
 plt.legend()
 
 # Save the plot to a PDF file
-plt.savefig('entropy.pdf', format='pdf', bbox_inches='tight')
+plt.savefig('entropy_new.pdf', format='pdf', bbox_inches='tight')
 
 plt.show()
 
